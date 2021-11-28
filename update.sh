b7fafb597e35a32a42c3c18254da8adbbb8868c4
jacksonhcnz <jackson.hansen.cane@gmail.com>
2021-11-29 09:27:35 +1300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,35 @@
+
+
+
+//add steamlink
+
+install steamlink
+
+<system>
+    <name>SteamLink</name>
+    <fullname>SteamLink</fullname>
+    <path>~/RetroPie/roms/amiga</path>
+    <extension>.sh .SH</extension>
+    <command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ amiga %ROM%</command>
+    <platform>SteamLink</platform>
+    <theme>amiga</theme>
+</system>
+
+//add moonlight
+
+
+//disable bluetooth
+
+
+//disable wifi
+
+
+//<system>
+//    <name>amiga</name>
+//    <fullname>Amiga</fullname>
+//    <path>~/RetroPie/roms/amiga</path>
+//    <extension>.sh .SH</extension>
+//    <command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ amiga %ROM%</command>
+//    <platform>amiga</platform>
+//    <theme>amiga</theme>
+//</system>
