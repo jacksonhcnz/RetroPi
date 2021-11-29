7da79da7943bcce14286150b51dfd4aeadc17402
jacksonhcnz <jackson.hansen.cane@gmail.com>
2021-11-29 15:15:38 +1300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,15 +5,28 @@
 
 install steamlink
 
+sudo apt-get install steamlink
+
+echo "<system>" >> /etc/emulationstation/es_systems.cfg
+echo "<name>SteamLink</name>" >> /etc/emulationstation/es_systems.cfg
+echo "<fullname>SteamLink</fullname>" >> /etc/emulationstation/es_systems.cfg
+echo "<path>~/RetroPie/roms/amiga</path>" >> /etc/emulationstation/es_systems.cfg
+echo "<extension>.sh .SH</extension>" >> /etc/emulationstation/es_systems.cfg
+echo "<command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ amiga %ROM%</command>" >> /etc/emulationstation/es_systems.cfg
+echo "<platform>SteamLink</platform>" >> /etc/emulationstation/es_systems.cfg
+echo "<theme>amiga</theme>" >> /etc/emulationstation/es_systems.cfg
+echo "</system>" >> /etc/emulationstation/es_systems.cfg
+
+
 <system>
-    <name>SteamLink</name>
-    <fullname>SteamLink</fullname>
-    <path>~/RetroPie/roms/amiga</path>
-    <extension>.sh .SH</extension>
-    <command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ amiga %ROM%</command>
-    <platform>SteamLink</platform>
-    <theme>amiga</theme>
-</system>
+    
+    
+    
+    
+    
+    
+    
+
 
 //add moonlight
 
